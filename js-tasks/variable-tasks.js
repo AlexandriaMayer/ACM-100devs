f2dc202e4700a4d371ba1e28e661e27e391af64a
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-07-11 13:52:54 -0500

    Add files via upload
    Tasks from javascript.info - will update as I make progress

---
+++ js-tasks/variable-tasks.js
@@ -0,0 +1,17 @@
+let admin, name;
+name = "John";
+admin = name;
+alert(name);
+// vscode is acting up on me, I had to run the code on https://www.programiz.com/javascript/online-compiler/
+// and it works!
+
+let home, visitor;
+home = "Earth";
+visitor = "Allie";
+console.log(visitor + " has returned to " + home);
+
+// turns out we're just using a regular string, not the actual user ID that would be grabbed through an actual script.
+
+// Unless this age or birthday needs to be retrieved in a larger database of ages and birthdays, I don't think they should be uppercased.
+// Age can be calculated by comparing current date to a birthday.
+
