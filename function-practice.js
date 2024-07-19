dda1b83c39b57489e8529d7f7dfe090359f9549c
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-07-19 16:34:01 -0500

    Create function-practice.js

---
+++ function-practice.js
@@ -0,0 +1,21 @@
+//create a function that adds two numbers and alerts the sum
+function sum(a, b){
+		// name a variable for c
+    let c = a + b;
+    alert(c);
+}
+//create a function that multiplys three numbers and console logs the product
+function multiply(a, b, c){
+    let d = a * b * c;
+    console.log(d)
+}
+//create a function that divides two numbers and returns the ???
+
+function divide(a, b){
+	// i think I did % wrong, cause I 
+    let c = a/b
+    return c
+}
+sum(1, 4);
+(multiply(3, -4));
+console.log(divide(9, 3))
