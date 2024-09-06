452f3f83bfade2c11bb4615dcb2a65005f6e9e13
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-09-05 22:53:34 -0500

    Update main.js

--- review-summer-games/js/main.js
+++ review-summer-games/js/main.js
@@ -1,8 +1,12 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
 
 function toodle(array){
-    //for(let i = 1; i<array.length();i++){let product = product * array[i]}
+    
     array.forEach(num => product *=num)
     alert(product)
+    /* alternatively    
+    for(let i = 1; i<array.length();i++){
+        product *= array[i]}
+        alert(product)*/
 }
 toodle([3, 2, 5, 2, 16, 94, -2, -4, 1, 7])
