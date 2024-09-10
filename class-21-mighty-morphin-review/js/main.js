110a1bd9d944e3e922d8e2e3ab835d7492d7e345
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-09-09 21:33:23 -0500

    Update main.js
    Fixed some of the functions

--- class-21-mighty-morphin-review/js/main.js
+++ class-21-mighty-morphin-review/js/main.js
@@ -10,49 +10,50 @@ console.log(favHoliday)
 
 let triPart = "Mizore"
 
-let tri = triPart(tripart.length-3)
-
-console.log(tri)
+console.log(triPart.slice(-3))
 
 // *Functions*
 // Create a function that takes in 5 numbers. Subtract all five from 100. Alert the absolute value of the difference. Call the function.
 
-function fiveOfThem (nums){
-    let oneHundred = 100
-    for (let i = 0; i<nums.length; i++){
+function fiveOfThem (n1, n2, n3, n4, n5){
+    let oneHundred = 100 - n1 - n2 - n3 -n4 -n5
+    /*for (let i = 0; i<nums.length; i++){
         let diff = (nums[i]-oneHundred)}
-    alert(oneHundred)
+        this was easier than expected*/
+    alert(Math.abs(oneHundred)) // ok i need to look at the mdn more
 }
 fiveOfThem([354, 1, 4 , 5, 4])
 
 // Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
 
-function threeOfThem(three){
-    console.log(three.max)
-    console.log(three.min)
+function threeOfThem(m1, m2, m3){
+    let max = (Math.max(m1, m2, m3))// ok i was close with the syntax
+    let min = (Math.min(m1, m2, m3))// oh it's three parameters, not an array with three elements
+    console.log(`The lowest number is ${min} and the highest number is ${max}`)
 }
-threeOfThem([55, 2, 9])
+threeOfThem(55, 2, 9)
 
 // *Conditionals*
 //Create a function that returns heads or tails randomly and as fairly as possible. Call the function.
 
-function coinFlip(flippy){
-    if (flippy%2===0){
-        alert("Heads")
-    }
-    else if (flippy%2!==0){
-        alert("Tails")
+function coinFlip(){
+    let result = Math.random()
+    if (result<.5){
+        return("Heads")
     }
     else{
-        alert("oops I dropped it")
+        return("Tails")
     }
-    return flippy
 }
-coinFlip(4)
+console.log(coinFlip())
 
 //*Loops*
 //Create a function that takes in a number. Console log the result of heads or tails using the previous function x times where x is the number passed into the function. Call the function.
 
 function hotToGo(flippy){
-    // wait I already did this
+    for(let i = 1; i<=flippy; i++){
+        let result = coinFlip()// ok I was close
+        console.log(result)
+    }
 }
+hotToGo(24)
