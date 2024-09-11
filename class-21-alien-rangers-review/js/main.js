7be43d370f39d3f554a42d46295e8a76cf6d977c
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-09-10 22:36:26 -0500

    Add files via upload
    Array practice

---
+++ class-21-alien-rangers-review/js/main.js
@@ -0,0 +1,46 @@
+//Arrays
+
+//Create and array of tv shows. Loop through and print each show to the console
+function television(){
+    let shows = ['The L Word', 'Sound!Euphonium', 'Bocchi the Rock!', 'K-On!', 'Nichijou', 'Laid Back Camp']
+    /*for (i=1; i<shows.length();i++){
+        console.log(shows[i])
+    }*/
+    shows.forEach(x=>console.log(x))
+}
+
+
+//Create and array of numbers
+//Return a new array of numbers that includes every even number from the previous Arrays
+
+function evenArray(nums){
+    if (nums[i]%2===0){
+        return nums
+    }
+    else{
+        console.log('how odd')
+    }
+    let newNums = nums
+
+}
+evenArray([48558, 294, 1, 4, 3, 4, 1050, 1, 9])
+
+//Create a function that takes in an array of numbers
+//Alert the sum of the second lowest and the second highest number
+
+function minAndMax(arr){
+    arr.sort(function(x, y){
+        return x - y
+    })
+    let sortedArr = [arr[0]]
+    let result = []
+    for (let i = i; i<arr.length; i++){
+        if (arr[i-1] !== arr[i]){
+            sortedArr.push(arr[i]);
+        }
+    }
+    result.push(sortedArr[1], sortedArr.length-2);
+
+    return result.join(',');
+}
+console.log(minAndMax([553, 3, 1, 5, 5, 27]));
