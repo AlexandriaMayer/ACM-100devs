39aaff574c3d20c0f515209425123af93940963f
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-09-16 22:57:10 -0500

    Update main.js

--- class-21-objects-dog-photos/js/main.js
+++ class-21-objects-dog-photos/js/main.js
@@ -35,6 +35,46 @@ function sortArr(arr3){
     let arr3=[1455, 343, 333, 5, 9, 1, -5, -9999, 404, 54]
     arr3.sort(function(a, b){return b-a});// apparently this will return the sort in descending order
     alert(arr3)}
+// eloquentjavascript 
 
+// The sum of a range
+
+/* Write a range function that takes two arguments, start and end, and returns an array containing all the numbers 
+from start up to and including end.*/
+function rangerOne(start, end){
+    this.start=start;
+    this.end=end;
+    console.log(Range(start, end));
+}
+// const texasRanger = new rangerOne(1, 21) // ok this is not working
+
+// reverse an array without arr.prototype.reverse()
+
+function reverseArray(arr4){
+    let rArr = new Array;
+    for (let i = arr4.length-1; i>=0; i--){
+        rArr.push(arr4[i]);
+    }
+    console.log(rArr);
+}
+
+reverseArray([5,3,2,1])
+// reverse an array in place
+function reverseArrayInPlace(arr5){
+    let i = 0,
+        n = arr2.length,
+        middle = Math.floor(n/2)
+        temp = null;
+    for (; i<middle; i+=1){
+        temp = arr5[i];
+        arr5[i]=arr5[n-1-i];
+        arr5[n-1-i]=temp;
+    }
+    console.log(arr5)
+}
+reverseArrayInPlace([9, 8, 7, 6, 5, 4])
+
+
+// oh my god it's case sensitive
 
 
