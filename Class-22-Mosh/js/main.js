1e4342d1314ff493b443341673907d4c3e574726
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-09-23 21:46:26 -0500

    Add files via upload
    Exercise is to create a stopwatch object. Noted down what I'll need, but it's just about time for bed.

---
+++ Class-22-Mosh/js/main.js
@@ -0,0 +1,8 @@
+console.log("private property? in my program?")
+
+const sw = new Stopwatch(){
+    // ok how to count time in JS?
+    // start. throw Error() if hit twice
+    // stop.  throw Error() if hit twice
+    // reset
+}
