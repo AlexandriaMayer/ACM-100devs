e1854bc41ebe085534591ef4adae62ddee405b17
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-09-26 22:26:06 -0500

    Create sumOfSquares.js

---
+++ codewars/sumOfSquares.js
@@ -0,0 +1,10 @@
+function squareSum(numbers)
+  {
+  let result = 0
+  for(i=0;i<numbers.length;i++){
+    result += numbers[i] ** 2
+  }
+    return result
+}
+
+// had to look it the solution, but I was close
