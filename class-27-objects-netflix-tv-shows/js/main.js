604ca27f5d2d03a9136b1733fa32fde1105cd13d
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-11-08 22:49:07 -0600

    Add files via upload
    Object practice

---
+++ class-27-objects-netflix-tv-shows/js/main.js
@@ -0,0 +1,22 @@
+//Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
+
+class NetflixShow {
+    constructor(name, epCount, rating, genre){
+        this.name = showName 
+        this.epCount = numOfEpisodes
+        this.rating = userRating
+        this.genre = showGenre
+
+
+}
+    playShow(showName){
+    console.log(`Now playing ${showName}.`)
+}
+    length(numOfEpisodes){
+        console.log(`${numOfEpisodes} long.`)
+    }
+    genre(showGenre){
+        console.log(`${showGenre}`)
+    }
+}
+let strangerThings = new NetflixShow('Stranger Things', 12, 4.9, 'mystery')
