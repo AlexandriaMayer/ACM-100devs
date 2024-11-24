5cea161c40651172e0996cc24d787580e0894a90
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-11-24 10:01:59 -0600

    Update stringToNum.js

--- codewars/stringToNum.js
+++ codewars/stringToNum.js
@@ -2,3 +2,9 @@ const stringToNumber = function(str){
   let num = Number(str)
   return num;
 }
+
+// 11/24/2024 convert "x years old" to number
+
+function getAge(inputString){
+  return Number(inputString[0])
+}
