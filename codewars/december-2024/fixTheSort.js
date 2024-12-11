e70824992d3ec02d67c3b2ff746c4d105c3b422c
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-12-10 20:38:54 -0600

    Create fixTheSort.js

---
+++ codewars/december-2024/fixTheSort.js
@@ -0,0 +1,5 @@
+function findSmallestInt(arr) {
+  let arrSorted = arr.sort((a,b)=> a - b)
+  return arrSorted[0]
+} 
+// ez, basically did this on another codewars problem. I need to add this to my Anki.
