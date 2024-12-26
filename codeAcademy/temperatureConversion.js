f78cb9ff68d0b2490751188cc004d9c0e03cab30
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-12-26 07:33:24 -0600

    Create temperatureConversion.js

---
+++ codeAcademy/temperatureConversion.js
@@ -0,0 +1,13 @@
+// uploading the code I entered in from my homework
+
+const kelvin = 9001
+// temperature in kelvin stored as int
+let celsius = kelvin - 273
+// celsius is 273 degrees less than kelvin
+let fahrenhiet = Math.floor(celsius * (9/5) + 32)
+// convertign from celsius to fahrenhiet, rounding the number
+let newton = Math.floor(celsius * (33/100));
+console.log(`The temperature is ${fahrenhiet} degrees Fahrenheit`)
+console.log(`It is ${newton} Newtons outside`)
+
+// ez
