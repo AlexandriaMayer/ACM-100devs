46a64379916c833dcb03dc15bb688fc1fe7a0156
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-12-26 10:03:16 -0600

    Update conditionals1.js

--- codeAcademy/conditionals1.js
+++ codeAcademy/conditionals1.js
@@ -1,4 +1,7 @@
-let sale = false
-if (sale == true){
-  console.log('Time to buy!')
+let sale = true;
+
+if(sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.')
 }
