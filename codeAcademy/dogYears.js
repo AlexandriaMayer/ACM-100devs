c8784fa589f6cbf8966cb450b641c42825883e22
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-12-26 09:54:45 -0600

    Create dogYears.js

---
+++ codeAcademy/dogYears.js
@@ -0,0 +1,16 @@
+let myAge = 25
+// it's my age in years. no comment required.
+let earlyYears = 2
+let eYears = earlyYears * 10.5
+let laterYears = myAge - 2 
+// calculations don't kick in until after 2 years
+let laterDogYears = laterYears * 4
+// the famous dog years
+console.log(earlyYears, laterYears, laterDogYears)
+myAgeInDogYears = earlyYears + laterDogYears
+// puppygirl mode achieved
+myName = "Kumiko"
+console.log(myName.toLowerCase())
+// arf arf
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+// if i was a dog i'd be very old
