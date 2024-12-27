71e87a113dcf9ee59cfd82ca590d18e20723fce3
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-12-26 20:49:51 -0600

    Create elseIf.js

---
+++ codeAcademy/elseIf.js
@@ -0,0 +1,18 @@
+let season = 'summer';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} 
+else if (season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!')
+}
+else if (season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.')
+} else if (season === 'summer') {
+  console.log('It\'s sunny and warm because it\'s summer!') }
+
+else {
+  console.log('Invalid season.');
+}
+
+// personally I would have the elif statements in the order of occurance 
