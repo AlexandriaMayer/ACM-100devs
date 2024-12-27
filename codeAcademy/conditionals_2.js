b7d122c229b6b89988a791367a841d3b8bfb592a
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2024-12-26 19:38:25 -0600

    Update conditionals_2.js

--- codeAcademy/conditionals_2.js
+++ codeAcademy/conditionals_2.js
@@ -6,3 +6,20 @@ if (mood === 'sleepy' && tirednessLevel > 8){
 } else {
   console.log('not bed time yet')
 }
+
+let wordCount = 5;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
