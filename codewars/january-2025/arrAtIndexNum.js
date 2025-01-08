6f89910bf9c3517827fb5afdcacb046553148673
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2025-01-07 20:10:46 -0600

    Create arrAtIndexNum.js

---
+++ codewars/january-2025/arrAtIndexNum.js
@@ -0,0 +1,15 @@
+function switchItUp(number){
+ let arr = ["Zero", 
+            "One", 
+            "Two", 
+            "Three", 
+            "Four", 
+            "Five", 
+            "Six",
+           "Seven",
+           "Eight",
+           "Nine"]
+  return arr[number]
+}
+
+// sad that I can't put in any string I want to
