b57ad2103d5f466b0e433ccf038fbc8fbc65f79f
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2025-01-21 18:53:04 -0600

    Create parseHex.js

---
+++ codewars/january-2025/parseHex.js
@@ -0,0 +1,5 @@
+function hexToDec(hexString){
+  return parseInt(hexString, 16)
+}
+
+// I feel really embarassed that I couldn't figure this out on my own, then I find the solution and it's just parseInt with a base of 16.
