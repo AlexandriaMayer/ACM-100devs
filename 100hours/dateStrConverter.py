c5e98e31cfc831aa7b0e9b97b86e909c334fe3f2
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2025-03-21 22:13:23 -0500

    Update dateStrConverter.py

--- 100hours/dateStrConverter.py
+++ 100hours/dateStrConverter.py
@@ -4,6 +4,8 @@ import pytz
 
 # had to close and re-open vscode
 
+
+
 def load ():
     print("sleepy times")
     df = pd.read_csv("C:/Users/alexi/OneDrive/Documents/sleep_times.csv")
@@ -12,11 +14,18 @@ def load ():
 
 df = load()
 
+def assign_tz(self):
+    cst = timezone('US/Central')
+    mst = timezone('US/Mountain')
+assign_tz(df)
+
 def toDateTime (self):
 
-    wakeup1 = self["date"] +" "+ self["wakeup1"]
+    # decided to find/replace CST/MST with the actual timezone names 
 
-    wakeup1_dt = datetime.strptime(wakeup1, '%m/%d/%Y %I:%M%p')
+    wakeup1 = self["date"] +" "+ self["wakeup1"] # + self[wu1_tz]
+
+    wakeup1_dt = pd.to_datetime(wakeup1, format='%m/%d/%Y %I:%M%p')
 
     # tbh I think I need to do this wet, then figure out how to make it dry
 
