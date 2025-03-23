e9e6ab89783ef0a2e21f37c062cfce00e0e1a718
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2025-03-22 22:53:41 -0500

    Update dateStrConverter.py

--- 100hours/dateStrConverter.py
+++ 100hours/dateStrConverter.py
@@ -4,6 +4,8 @@ import pytz
 
 # had to close and re-open vscode
 
+# this is really dirty, am going to refactor it following kouhai's advice
+
 
 
 def load ():
@@ -15,20 +17,49 @@ def load ():
 df = load()
 
 def assign_tz(self):
-    cst = timezone('US/Central')
-    mst = timezone('US/Mountain')
+    todaynow = datetime.now(tz=pytz.timezone('US/Central'))
+    print(todaynow)
+
 assign_tz(df)
 
 def toDateTime (self):
 
     # decided to find/replace CST/MST with the actual timezone names 
 
-    wakeup1 = self["date"] +" "+ self["wakeup1"] # + self[wu1_tz]
+    # for now, I am coding WET and FAST. I can refactor it later with classes/constructors to
+    # make the code dry.
 
-    wakeup1_dt = pd.to_datetime(wakeup1, format='%m/%d/%Y %I:%M%p')
+    wakeup1 = self["date"] +" "+ self["wakeup1"] +" "+ self["wu1_tz"]
 
+    wakeup1_dt = pd.to_datetime(wakeup1, format='%m/%d/%Y %I:%M%p %z')
+
+    back_to_bed = self["date"] + " " + self["backToBed"] + " " + self["btb_tz"]
+    back_to_bed_dt = pd.to_datetime(back_to_bed, format='%m/%d/%Y %I:%M%p %z')
+
+    wakeup2 = self["date"] +" "+ self["wakeup2"] + " " + self["wu2_tz"]
+    wakeup2_dt = pd.to_datetime(wakeup2, format='%m/%d/%Y %I:%M%p %z')
+
+    bedtime = self["date"] +" "+self["bedtime"] + " " + self["bt_tz"]
+    bedtime_dt = pd.to_datetime(bedtime, format='%m/%d/%Y %I:%M%p %z')
+    self["sleep_time_1"] = 0
+    self["sleep_time_2"] = 0
+    self["sleep_time_1"][1:] = wakeup1_dt.values[1:] - bedtime_dt.values[:-1]
+    self["sleep_time_2"][1:] = wakeup2_dt.values[1:] - back_to_bed_dt.values[:-1]
+    self["sleep_total_time"] = 0
+    self["sleep_total_time"] = self["sleep_time_1"] + self["sleep_time_2"]
     # tbh I think I need to do this wet, then figure out how to make it dry
 
-    print(wakeup1_dt)
+    print(f'Wakeup 1: {wakeup1_dt}')
+    print(f'Back to Bed: {back_to_bed_dt}')
+    print(f'Wakeup 2: {wakeup2_dt}')
+    print(f'Bedtime: {bedtime_dt}')
+    print(f'Sleep 1 time: {self["sleep_time_1"]}')
+    print(f'Sleep 2 time: {self["sleep_time_2"]}')
+    print(f'Total Sleep Time: {self["sleep_total_time"]}')
+
+    #for e in self.itertuples():
+    #    print(e)
+
+    # it's working, but I'll need to figure out how to do conditionals for excluding NaT values
 
 toDateTime(df)
