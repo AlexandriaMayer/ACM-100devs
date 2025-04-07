6b3c04d1e410a9f0a2407973f6c6219142303987
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2025-04-06 20:21:29 -0500

    Add files via upload

---
+++ thursday-the-20th/js/script.js
@@ -0,0 +1,12 @@
+
+function compareDate(inputDate, endDate, currentDate, includeLastDay, submission){
+    // option to include or exclude end date
+    // if endDate is before inputDate,  
+}
+
+function thursdayThe20th(){
+    // for loop?
+    // if date[i][dayOfWeek] = "Thursday" && date[i][day] = 20, console.log("Thursday the 20th!")
+    // if date[i][dayOfWeek] = "Thursday", console.log("Thursday")
+    // if date[i][day] = 20, console.log("the 20th!")
+}
