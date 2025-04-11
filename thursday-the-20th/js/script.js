bfd50f49a765fd13201e5b2c30f078b7a560cbe1
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2025-04-11 13:36:41 -0500

    Update script.js
    Current state of the script

--- thursday-the-20th/js/script.js
+++ thursday-the-20th/js/script.js
@@ -1,4 +1,27 @@
 
+document.querySelector('#inputDate').addEventListener('click',inputDate)
+document.querySelector('#endDate').addEventListener('click',endingDate)
+document.querySelector('#currentDate').addEventsListener('click', currentDate)
+document.querySelector('#includeLastDay').addEventsListener('click', includeLastDay)
+
+// 4/11/2025 this isn't working and needs so much help
+
+function inputDate(){
+    let inputDate = new Date(document.querySelector('#inputDate').value)
+    console.log(inputDate)
+}
+function endingDate(){
+    let endingDate = new Date(document.querySelector('#endDate').value)
+    console.log(endingDate)
+}
+function currentDate(){
+    let today = document.querySelector('#currentDate').addEventsListener('click')
+    return today
+}
+function includeLastDaty(){
+
+}
+
 function compareDate(inputDate, endDate, currentDate, includeLastDay, submission){
     // option to include or exclude end date
     // if endDate is before inputDate,  
