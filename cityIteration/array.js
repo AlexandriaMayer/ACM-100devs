e04df4681d1df4ce096da1c1b54fb7b13da23722
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2025-06-18 19:38:43 -0500

    Update array.js

--- cityIteration/array.js
+++ cityIteration/array.js
@@ -1,21 +1,29 @@
 // Online Javascript Editor for free
 // Write, Edit and Run your Javascript code using JS Online Compiler
 
-function cityCounter(pop, growth){
+function cityCounter(totalPop, growth){
     // pop is a number, a count that is measured up to
     // growth is 1 + a percentage that the number grows by
     let count = 0,
         arr = [],
         map = {},
         x = 0,
-        y = 0;
-    for(let i = 100; i < pop; 0){
-        arr.push([i, x, y])
-        i = i + growth
+        y = 0
+        accPop = 0;
+    for(let i = 1; accPop < totalPop; i++){
+        arr.push([i, accPop, x, y])
+        accPop = accPop + growth
+        if (i % 2 == 0){
+            x++
+        }
+        if (i % 2){
+            y++
+        }
     }
     console.log(count, arr, arr[arr.length-1])
 }
 
 cityCounter(1000, 1.25)
+// 6/18/25 pausing, got the pop growth formula, but now I need to figure how to iterate over location
 
-// pausing, got the pop growth formula, but now I need to figure how to iterate over location
+//  6/18/25 changed i to act as a primary key, population is a different field. basic x & y iteration added
